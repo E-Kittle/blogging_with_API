774d305fbfe08e2c84d77f1e3a8ee98305c05224
Elisabeth Kittle <elisabethkittle@gmail.com>
2021-08-03 14:05:28 -0400

    Added localhost to cors

--- app.js
+++ app.js
@@ -29,7 +29,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
